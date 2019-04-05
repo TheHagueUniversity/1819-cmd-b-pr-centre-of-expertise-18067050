f1cfb95b322205bdcaefc318a87673aeedd3bc21
18067050 <48981909+18067050@users.noreply.github.com>
2019-04-05 23:24:28 +0200

    tabs afgemaakt, footer begonnen.

--- projectc/scripts/script.js
+++ projectc/scripts/script.js
@@ -1,13 +1,24 @@
-function openCity(evt, cityName) {
+function openTab(evt, tabName) {
   var i, tabcontent, tablinks;
+
   tabcontent = document.getElementsByClassName("tabcontent");
+
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
+
   tablinks = document.getElementsByClassName("tablinks");
+
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+
+  document.getElementById(tabName).style.display = "block";
+
   evt.currentTarget.className += " active";
+
+
 }
+
+// Open het event tab wanneer de pagina laadt
+document.getElementById('eventButton').click();
